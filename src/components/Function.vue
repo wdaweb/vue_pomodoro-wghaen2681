5f6b8999f8ec44ac3d806e835611291092c7c796
Neil Wang <wghaen2681@gmail.com>
2021-07-18 19:34:47 +0800

    1

--- src/components/Function.vue
+++
@@ -1,7 +0,0 @@
-<template>
-<div id="funtion">
-    <router-link to="./list" id="btn1"></router-link>
-    <router-link to="./chart" id="btn2"></router-link>
-    <router-link to="./settings" id="btn3"></router-link>
-</div>
-</template>
--- src/components/Header.vue
+++
@@ -1,6 +0,0 @@
-<template lang="pug">
-router-link(to="./")#header
-    #logo
-    div
-    #sublogo
-</template>
